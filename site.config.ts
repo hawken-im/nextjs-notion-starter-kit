393c5f587deccc99199362882b23aa8761247bf7
hawken <askhaokun@gmail.com>
2023-04-02 14:25:22 +0800

    new logo

--- site.config.ts
+++ site.config.ts
@@ -17,8 +17,8 @@ export default siteConfig({
   description: 'Welcome to funAI.world',
 
   // social usernames (optional)
-  //twitter: 'transitive_bs',
-  //github: 'transitive-bullshit',
+  twitter: 'whatajerrrk',
+  github: 'hawken-im',
   //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
@@ -50,16 +50,12 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '41aba75634154be6af4c711d742ac524'
+    }
+  ]
 })
